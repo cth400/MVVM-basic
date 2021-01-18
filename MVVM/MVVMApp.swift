defa2c23321e44f62d9057d44e0d7f490b0c19b9
CHOITAEHYUNG <cth400@naver.com>
2021-01-18 17:29:56 +0100

    Initial Commit

---
+++ MVVM/MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMApp.swift
+//  MVVM
+//
+//  Created by TAEHYUNG CHOI on 18/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
