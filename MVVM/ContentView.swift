083528bc252f5fe179c09d826322a7ef74acb080
CHOITAEHYUNG <cth400@naver.com>
2021-01-18 19:09:17 +0100

    choi

--- MVVM/ContentView.swift
+++ MVVM/ContentView.swift
@@ -7,10 +7,54 @@
 
 import SwiftUI
 
+//Model
+struct Person {
+    var name: String
+    var birthday: Date
+}
+
+//ViewModel
+class ViewModel: ObservableObject {
+   @Published private var person = Person(name: "홍길동", birthday: Date())
+    
+    
+    var name: String {
+        person.name
+    }
+    func changeName(_ name: String){
+        person.name = name
+    }
+    func birthDay(_ day : Date) -> String{
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        let birthDay = formatter.string(from: day)
+        return birthDay
+    }
+}
+//View
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    @State var birthDay = Date()
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Text(viewModel.name)
             .padding()
+            Text("\(viewModel.birthDay(birthDay))")
+            .padding()
+            
+            Button(action: {
+                
+            viewModel.changeName("김태희")
+            }, label: {
+                Text("Change Name")
+            })
+            
+            DatePicker("생일" , selection: $birthDay , displayedComponents : .date )
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding()
+
+        }.font(.largeTitle)
     }
 }
 
